275d3824d666836a7aa6daea1846ffc415154315
Sokol2020 <mr.sokolsergey@gmail.com>
2021-10-01 11:00:32 +0300

    Lesson01

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'Заголовок';
+let screens = 'Скриншоты';
+let screenPrice = 'Скриншот цены';
+const rollback = 'Откат';
+let fullPrice = 'Полная цена';
+const adaptive = 'Адаптив';
+
+alert('Всплывающее окно');
+console.log('Проверка работы консоли');
