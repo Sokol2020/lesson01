80eec9013a642c9f11ff0aa5b3758843fc0832be
Sokol2020 <mr.sokolsergey@gmail.com>
2021-10-02 19:16:43 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let title = 'Заголовок';
-let screens = 'Скриншоты';
-let screenPrice = 'Скриншот цены';
-const rollback = 'Откат';
-let fullPrice = 'Полная цена';
-const adaptive = 'Адаптив';
+let title = 'lesson02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 28;
+const rollback = 14;
+let fullPrice = 150000;
+const adaptive = true;
 
-alert('Всплывающее окно');
-console.log('Проверка работы консоли');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+
+console.log(screens.toLowerCase().split(","));
+
+console.log(fullPrice * (rollback/100));
+
+
