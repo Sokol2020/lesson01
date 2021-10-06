95d3793124126c14c0a0a8d5a15348f636f4955c
Sokol2020 <mr.sokolsergey@gmail.com>
2021-10-06 18:41:59 +0300

    fix script.js console.log and let/const

--- script.js
+++ script.js
@@ -1,56 +1,18 @@
 'use strict';
+const rollback = 14;
 
-let title = 'lesson02';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 28;
-let rollback = 14;
-let fullPrice = 150000;
-let adaptive = true;
+const title = prompt("Как называется наш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
 
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = prompt("Сколько это будет стоить?");
+let fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " гривен");
-console.log("Стоимость разработки сайта " + fullPrice + " гривен");
-
-console.log(screens.toLowerCase().split(","));
-
-console.log(fullPrice * (rollback/100));
-
-
-title = prompt("Как называется наш проект?");
-console.log(title);
-
-screens = prompt("Какие типы экранов нужно разработать?");
-console.log(screens);
-
-screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screenPrice);
-
-adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptive);
-
-
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service1);
-
-let servicePrice1 = prompt("Сколько это будет стоить?");
-console.log(servicePrice1);
-
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service2);
-
-let servicePrice2 = prompt("Сколько это будет стоить?");
-console.log(servicePrice2);
-
-fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);
-console.log(fullPrice);
-
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 console.log(servicePercentPrice);
 
 if(fullPrice > 30000){
@@ -66,3 +28,11 @@ else if (fullPrice < 0){
   console.log("Что то пошло не так");
 }
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " гривен");
+console.log("Стоимость разработки сайта " + fullPrice + " гривен");
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback/100));
