75a7e7cb3decc3ddea5a1159d4166aedf627fef9
Sokol2020 <mr.sokolsergey@gmail.com>
2021-11-19 16:02:06 +0200

    fix times.js .slice(-2) and setInterval

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,35 +1,38 @@
 const timer = (deadline) => {
-  const timerHours = document.getElementById('timer-hours')
-  const timerMinutes = document.getElementById('timer-minutes')
-  const timerSeconds = document.getElementById('timer-seconds')
-
+  const timerHours = document.getElementById("timer-hours");
+  const timerMinutes = document.getElementById("timer-minutes");
+  const timerSeconds = document.getElementById("timer-seconds");
 
   const getTimeRemaining = () => {
-    let dateStop = new Date(deadline).getTime()
-    let dateNow = new Date().getTime()
+    let dateStop = new Date(deadline).getTime();
+    let dateNow = new Date().getTime();
+    let timeRemaining = (dateStop - dateNow) / 1000;
+    let hours = Math.floor(timeRemaining / 3600);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
 
-    let timeRemaining = (dateStop - dateNow) / 1000
-    let hours = Math.floor(timeRemaining / 3600)
-    let minutes = Math.floor((timeRemaining / 60) % 60)
-    let seconds = Math.floor(timeRemaining % 60)
-
-    return { timeRemaining, hours, minutes, seconds }
-  }
-
+    return { timeRemaining, hours, minutes, seconds };
+  };
 
   const updateClock = () => {
+    let getTime = getTimeRemaining();
+    timerHours.textContent = ("0" + getTime.hours).slice(-2);
+    timerMinutes.textContent = ("0" + getTime.minutes).slice(-2);
+    timerSeconds.textContent = ("0" + getTime.seconds).slice(-2);
+  };
 
-    let getTime = getTimeRemaining()
-
-    timerHours.textContent = getTime.hours
-    timerMinutes.textContent = getTime.minutes
-    timerSeconds.textContent = getTime.seconds
-
-    if(getTime.timeRemaining > 0) {
-      setTimeout(updateClock, 1000)
-    }
+  if (getTimeRemaining > 0) {
+    updateClock();
   }
-  updateClock()
-}
 
-export default timer
+  setInterval(() => {
+    let getTime = getTimeRemaining();
+    if (getTime.timeRemaining > 0) {
+      updateClock();
+    } else if (getTime.timeRemaining === 0) {
+      clearInterval(updateClock);
+    }
+  }, 1000);
+};
+
+export default timer;
