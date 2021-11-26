4574b2f42cb19a2ef383007b1cc59aff9d1a6d51
Sokol2020 <mr.sokolsergey@gmail.com>
2021-11-26 22:21:08 +0200

    add slider.js + fix srcoll.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import smoothScroll from './modules/smoothScroll'
 import formsValidation from './modules/formsValidator'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer(`22 december 2021`)
 menu()
@@ -11,3 +12,4 @@ modal()
 smoothScroll()
 formsValidation()
 tabs()
+slider()
